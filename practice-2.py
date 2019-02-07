0f107f90c1c259b00a44819570407ba974387cae
paddumelanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2019-02-07 13:24:33 -0800

    Create practice-2.py

---
+++ practice-2.py
@@ -0,0 +1,36 @@
+# Python Practice 2
+# Author: Paddu Melanahalli
+#
+# +: plus
+# -: minus
+# /: slash
+# *: asterisk
+# %: mod
+# <: less-than
+# >: greater-than
+# <=: less-than-equal
+# >=: greater-than-equal
+# Notice how the operations are missing? After you type in the code for this exercise, go back and figure out what each of these does and complete the table.
+# For example,+does addition.
+
+print("I will now count my chickens:")
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+print("Now I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that 3 + 2 < 5 -7?")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+print("How about some more.")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# STUDY DRILLS
+# Above each line, use the # to write a comment to yourself explaining what the line does.
+# Find something you need to calculate and write a new.py file that does it.
+# Rewrite to use floating point numbers so it’s more accurate. 20.0 is floating point
+# How does % work? Another way to say it is, “X divided by Y with J remaining.”
+# For example, “100 divided by 16 with 4 remaining.” The result of % is the J part, or the remaining part.
