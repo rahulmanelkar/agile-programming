2b9425cd2f308876c8a0422fc2e84c5e932ded8b
paddumelanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2019-02-07 13:14:52 -0800

    Create practice-1.py

---
+++ practice-1.py
@@ -0,0 +1,15 @@
+# Python Practice 1
+# Author: Paddu Melanahalli
+#
+print("Hello World!")
+print("Hello Again")
+print("I like typing this for my UCSC class.")
+print("This is a fun class.")
+print('Yay! Printing is fun.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+#
+#1. Make your script print another line.
+#2. Make your script print only one of the lines.
+#3. Put a # (octothorpe/hash) character at the beginning of a line. What did it do? Try to find out what this character does.
+#
