1f05a57d7498ec68878f493f337cda9bac653ba9
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2019-02-17 12:25:29 -0800

    Create string-examples.py

---
+++ string-examples.py
@@ -0,0 +1,14 @@
+print ("Enter your name:")
+x=input()
+print ("Hello, "+x)
+
+a= "  Hello World!  "
+print (a[3])
+print (a[2:5])
+print (a.strip())
+print (len(a))
+print (a.lower())
+print (a.upper())
+print (a.replace("H", "J"))
+a= "Hello, World!"
+print (a.split(","))
