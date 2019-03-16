fb2326afb144018d79059fd8d6e78895710a17b6
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2019-03-16 09:05:07 -0700

    Create dictionary-example.py

---
+++ dictionary-example.py
@@ -0,0 +1,71 @@
+# Python Practice 10
+#
+#A dictionary is another example of a data structure, and, like a list,
+#it is one of the most commonly used data structures in programming.
+#A dictionary is used to map or associate things you want to store to keys you need to get them
+# create a mapping of state to abbreviation
+
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# print out some cities
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+# print some states
+print('-' * 10)
+print("Michigan's abbreviation is: ", states['Michigan'])
+print("Florida's abbreviation is: ", states['Florida'])
+
+# do it by using the state then cities dict
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+# print every state abbreviation
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+# print every city in state
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+# now do both at the same time
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+# safely get a abbreviation by state that might not be there
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+# get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
+
+#1. Do this same kind of mapping with cities and states/regions in your country or some other country.
+#2. Find the Python documentation for dictionaries and try to do even more things to them.
+#3.. Find out what you can’t do with dictionaries. A big one is that they do not have order, so try playing with that.
