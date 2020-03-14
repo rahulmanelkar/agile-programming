5fe4498dc6c15467630c17bb614ea3f2a7dd3829
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2020-03-13 18:43:05 -0700

    Create rotation-cypher.py

---
+++ rotation-cypher.py
@@ -0,0 +1,16 @@
+def encrypt(string, shift):
+    cipher = ''
+    for char in string:
+        if char == '':
+            cipher = cipher + char
+        elif char.isupper():
+            cipher = cipher + chr((ord(char) + shift - 65)%26 + 65)
+        else:
+            cipher = cipher + chr((ord(char) + shift - 97)%26 + 97)
+    return cipher
+
+text = input("Enter string: ")
+s = int(input ("Enter Shift number: "))
+print("Original string", text1)
+
+print ("After Encryption : ", encrypt(text,s))
