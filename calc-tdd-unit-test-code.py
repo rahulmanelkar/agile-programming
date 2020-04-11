970261edd314c486adfdc0b218ddaa4517bf2265
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2020-04-10 19:14:08 -0700

    Update calc-tdd-unit-test-code.py

--- calc-tdd-unit-test-code.py
+++ calc-tdd-unit-test-code.py
@@ -2,14 +2,34 @@ import unittest
 from calcTddProductionCode import *
 
 class SimpleTest(unittest.TestCase):
-    def testadd(self):
+
+# ADD
+    def testadd1(self):
         self.assertEqual(add(3,5),8)
 
-    def testadd1(self):
+    def testadd2(self):
         self.assertEqual(add(3.5,5), 8.5)
 
-    def testadd2(self):
+    def testadd3(self):
         self.assertEqual(add("One", "Two"), "OneTwo")
+# SUB
+    def testsub4(self):
+        self.assertEqual(sub(5, 3), 2)
+
+    def testsub5(self):
+        self.assertEqual(sub(5.5, 2.5), 3.0)
+# MULTIPLY
+    def testMultiply6(self):
+        self.assertEqual(multiply (3,2),6)
+
+    def testMultiply7(self):
+        self.assertEqual(multiply (3.5,2.5),8.75)
+#DiVIDE
+    def testDivide8(self):
+        self.assertEqual(divide (8,2),4)
+
+    def testDevide9(self):
+        self.assertEqual(divide (25.0,5),5.0)
 
 if __name__ == '__main__':
     unittest.main()
