417cca705be850c8a5a8f6b9a15501c568f67020
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2020-04-10 19:14:43 -0700

    Update calcTddProductionCode.py

--- calcTddProductionCode.py
+++ calcTddProductionCode.py
@@ -1,14 +1,16 @@
 def add(a,b):
+    return a+b;
     '''
     if type(a) == int and type(b) == int:
         return a+b
     else:
         raise TypeError("Invalid type: {} and {}".format(type(a),
                                                      type(b)))
-    '''
-    pass
+'''
+    #pass
 
 def sub(a,b):
+    return a-b
     '''
     assert type(a) is not IntType, "a is not integer: %r" % a
     assert type(b) is StringType, "name is a string: %r" % name
@@ -16,7 +18,9 @@ def sub(a,b):
     #pass
 
 def multiply(a,b):
+    return a*b
     pass
 
 def divide(a,b):
+    return a/b
     pass
