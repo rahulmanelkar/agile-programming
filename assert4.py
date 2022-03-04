a83eb920b09d803142df7dd8edc44dd466858fa6
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-03-03 19:27:44 -0800

    Create assert4.py

---
+++ assert4.py
@@ -0,0 +1,19 @@
+# initializing number
+a = 4
+b = 0
+
+# using assert to check for 0
+print("The value of a / b is : ")
+assert b != 0, "Divide by 0 error"
+print(a / b)
+
+
+
+# Test case 2
+x = "hello"
+
+#if condition returns False, AssertionError is raised:
+assert x == "goodbye", "x should be 'hello'"
+
+# Test case 3
+x = "hello"
