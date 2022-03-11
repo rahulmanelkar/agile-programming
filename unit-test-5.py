219c445697009a21fae632d282d0626e0841ba9b
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-03-10 18:44:26 -0800

    Update unit-test-5.py

--- unit-test-5.py
+++ unit-test-5.py
@@ -2,17 +2,17 @@ import unittest
 class TestFixtures(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print('called once before any tests in class')
+        print('\ncalled class method - once before any tests in class')
     @classmethod
     def tearDownClass(cls):
-        print('\ncalled once after all tests in class')
+        print('\ncalled class method -  once after all tests in class')
     def setUp(self):
         self.a=10
         self.b=20
         name=self.shortDescription()
-        print('\n',name)
+        print('\n setup : ',name)
     def tearDown(self):
-        print('\nend of test',self.shortDescription())
+        print('\nTeardown : ',self.shortDescription())
 
     def test1(self):
         """One"""
