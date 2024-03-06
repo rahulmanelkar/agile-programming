1e369da9d7376ba194661e410e91f43b60b285c1
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2024-03-05 21:24:48 -0800

    Create tdd-example-1.py

---
+++ tdd-example-1.py
@@ -0,0 +1,11 @@
+import unittest
+
+def hello_world():
+    return 'hello world'
+
+class MyFirstTests(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello_world(), 'hello world')
+
+if __name__ == '__main__':
+    unittest.main()
